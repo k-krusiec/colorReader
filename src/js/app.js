5d8d70dd5ab58e558eeec9e5540fcfd88ce557fb
Karol Krusiec <karol@krusiec.com>
2017-09-24 20:29:19 +0200

    added hex to rgb conversion

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import { ColorModule } from './modules/colorModule.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   var form = document.querySelector('.form');
+  var outputBox = document.querySelector('.output-box');
 
-  ColorModule.init({form: form});
-  console.log(ColorModule);
+  ColorModule.init({form: form, outputBox: outputBox});
 });
--- src/js/modules/colorModule.js
+++ src/js/modules/colorModule.js
@@ -1,14 +1,66 @@
 var ColorModule = (function () {
   var options = {};
+  var rgbColor = {};
+
+  var hexToRgb = function (color, outputs) {
+    rgbColor.r = parseInt(color.substring(0, 2), 16);
+    rgbColor.g = parseInt(color.substring(2, 4), 16);
+    rgbColor.b = parseInt(color.substring(4, 6), 16);
+
+    outputs[1].lastElementChild.innerText = 'R: ' + rgbColor.r + ' G: ' + rgbColor.g + ' B: ' + rgbColor.b;
+  };
+
+  var setOutputColor = function (color, outputColor) {
+    outputColor.style.backgroundColor = '#' + color;
+  };
+
+  var testColorInput = function (input, outputColor, outputs) {
+    var color = input.value;
+    var isValid = true;
+
+    color = color.replace(/#/g, '');
+
+    if (!color) {
+      console.log('to pole jest wymagane');
+      isValid = false;
+    } else if (color.length !== 6) {
+      console.log('numer hex powinien mieć 6 znaków');
+      isValid = false;
+    }
+
+    if (isValid) {
+      hexToRgb(color, outputs);
+      setOutputColor(color, outputColor)
+      return true;
+    }
+  };
+
+  var prepareElements = function () {
+    var form = options.form;
+    var colorInput = options.form.querySelector('[name="enter-color"]');
+    var outputColor = options.output.querySelector('.output-color');
+    var outputs = options.output.querySelectorAll('.output');
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      testColorInput(colorInput, outputColor, outputs);
+      // this.submit();
+    });
+  };
+
   var init = function (_options) {
     options = {
-      form: _options.form || null
+      form: _options.form || null,
+      output: _options.outputBox || null
     };
 
     if (_options.form === null || _options.form === undefined || _options.form === 0) {
       console.warn('ColorModule: Źle przekazany kalendarz!');
       return false;
     }
+
+    prepareElements();
   };
   return {
     init: init
